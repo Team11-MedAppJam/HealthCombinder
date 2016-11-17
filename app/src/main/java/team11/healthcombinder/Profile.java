0501f0644d7f2226d8d1a52d06889460131825b6
chanhuu29 <chanhuu29@yahoo.com>
2016-11-17 12:56:24 -0800

    Profile
    Created profile UI

---
+++ app/src/main/java/team11/healthcombinder/Profile.java
@@ -0,0 +1,17 @@
+package team11.healthcombinder;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_profile);
+    }
+}
