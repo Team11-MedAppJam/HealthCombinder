e79b4a831b36682280b674d3b5dad51b8e3ff154
chanhuu29 <chanhuu29@yahoo.com>
2016-11-19 00:53:45 -0800

    Complete Profile
    Completed profile and its sub-activities.
    Created popup windows for change password and email.
    Annual checklist page

--- app/src/main/java/team11/healthcombinder/Profile.java
+++ app/src/main/java/team11/healthcombinder/Profile.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Profile extends AppCompatActivity {
 
@@ -24,5 +25,22 @@ public class Profile extends AppCompatActivity {
                 startActivity(new Intent(Profile.this, popup.class));
             }
         });
+
+        ImageButton b_email = (ImageButton) findViewById(R.id.edit_email);
+        b_email.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(Profile.this, change_email.class));
+            }
+        });
+    }
+    public void openHistoryChecklist(View view) {
+        Intent intent = new Intent(this, annual_history_checklist.class);
+        startActivity(intent);
+    }
+
+    public void toHomePage(View view){
+        Intent intent = new Intent(this, Timeline.class);
+        startActivity(intent);
     }
 }
