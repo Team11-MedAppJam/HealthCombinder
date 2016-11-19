cc0e0d2e4d358bd0f45b62b2a58e61bc0f1f935c
Cesar Alex Diaz <calexdiaz143@gmail.com>
2016-11-19 02:05:43 -0800

    Create addCard gui
    --May need to add rectangles around submission fields in the future

---
+++ app/src/main/java/team11/healthcombinder/addcard.java
@@ -0,0 +1,13 @@
+package team11.healthcombinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addcard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcard);
+    }
+}
